be6edc0213bc36727b821c3f12cd526c1db9dffc
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2023-01-31 19:33:43 +0800

    [WIP] changed to using the src folder
    Description
    Moved everything there, deciding how to code it out

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import Logo from "@/assets/logo.png";
-import HelloWorld from "@/components/HelloWorld/HelloWorld";
 import { Helmet } from "react-helmet";
+import faviconUrl from "@/assets/favicon.png";
 
 export default function App() {
   return (
     <main>
       <Helmet>
         <meta charSet="UTF-8" />
+        <link rel="icon" type="image/png " href={faviconUrl} />
         <title>OES-RSFG</title>
       </Helmet>
     </main>
