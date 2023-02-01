78bc4c20cf7922bc49ae15f1bd0247aed0d39688
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2023-02-01 15:48:53 +0800

    [WIP] added tabs for the forms

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,4 +1,5 @@
 import AppBar from "@mui/material/AppBar";
+import faviconUrl from "@/assets/favicon.png";
 import Grid from "@mui/material/Unstable_Grid2";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -6,23 +7,38 @@ import IconButton from "@mui/material/IconButton";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import { useContext } from "react";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import { Link as RouterLink } from "react-router-dom";
 
 import { ColorModeContext } from "@/App";
 import { useTheme } from "@mui/material/styles";
+import { Avatar } from "@mui/material";
 
-export default function DefaultLayout() {
+export declare interface DefaultLayoutProps {
+  children?: React.ReactNode;
+}
+
+export default function DefaultLayout({ children }: DefaultLayoutProps) {
   const theme = useTheme();
   const colorMode = useContext(ColorModeContext);
 
   return (
     <main>
       <Grid container spacing={0}>
-        <Grid sm={12}>
+        <Grid xs={12}>
           <AppBar position="static">
             <Toolbar>
-              <Typography variant="h6" component="div" sx={{ mr: "auto" }}>
-                OES-RSFG
-              </Typography>
+              <Link component={RouterLink} to="/">
+                <Avatar sx={{ mr: 1 }} alt="MES Icon" src={faviconUrl} variant="square" />
+              </Link>
+              <Box sx={{ mr: "auto" }}>
+                <Link component={RouterLink} to="/" color="inherit" underline="none">
+                  <Typography variant="h6" component="div">
+                    OES-RSFG
+                  </Typography>
+                </Link>
+              </Box>
               <IconButton color="inherit" onClick={colorMode.toggleColorMode}>
                 {theme.palette.mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
               </IconButton>
@@ -30,6 +46,7 @@ export default function DefaultLayout() {
           </AppBar>
         </Grid>
       </Grid>
+      {children}
     </main>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,39 @@
 import DefaultLayout from "@/layouts/DefaultLayout/DefaultLayout";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Tab, Typography } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+import { Box } from "@mui/system";
+import { useState } from "react";
 
 export default function Home() {
-  return <DefaultLayout></DefaultLayout>;
+  const [tabFormValue, setTabFormValue] = useState<"report-sick" | "outcome">("report-sick");
+  return (
+    <DefaultLayout>
+      <Grid container sx={{ my: 2 }}>
+        <Grid xs={12}>
+          <Typography variant="h4" textAlign="center">
+            OES Report Sick Format Generator (OES-RSFG)
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid xs={12}>
+          <TabContext value={tabFormValue}>
+            <Box sx={{ mx: { sm: 0, md: 2 } }}>
+              <TabList
+                onChange={(_, value) => {
+                  setTabFormValue(value);
+                }}
+              >
+                <Tab label="Report Sick" value="report-sick" />
+                <Tab label="Outcome" value="Outcome" />
+              </TabList>
+              <TabPanel value="report-sick">Report Sick</TabPanel>
+              <TabPanel value="outcome"></TabPanel>
+            </Box>
+          </TabContext>
+        </Grid>
+      </Grid>
+    </DefaultLayout>
+  );
 }
