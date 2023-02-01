0875db368414e21e001b7bce1ff4e58c88d118a2
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2023-02-01 11:51:20 +0800

    [feat] added in colortheme and toggle dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,50 @@ import faviconUrl from "@/assets/favicon.png";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "@/pages/Home/Home";
 import { CssBaseline } from "@mui/material";
+import { useState, useMemo, createContext } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {
+    // If the toggle color mode breaks for whatever reason, Empty func
+  },
+});
 
 export default function App() {
+  const [mode, setMode] = useState<"light" | "dark">("dark");
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
   const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+
   return (
-    <CssBaseline>
-      <Helmet>
-        <meta charSet="UTF-8" />
-        <link rel="icon" type="image/png " href={faviconUrl} />
-        <title>OES-RSFG</title>
-      </Helmet>
-      <RouterProvider router={router} />
-    </CssBaseline>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <Helmet>
+            <meta charSet="UTF-8" />
+            <link rel="icon" type="image/png " href={faviconUrl} />
+            <title>OES-RSFG</title>
+          </Helmet>
+          <RouterProvider router={router} />
+        </CssBaseline>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -2,9 +2,18 @@ import AppBar from "@mui/material/AppBar";
 import Grid from "@mui/material/Unstable_Grid2";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { useContext } from "react";
+
+import { ColorModeContext } from "@/App";
+import { useTheme } from "@mui/material/styles";
 
 export default function DefaultLayout() {
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
+
   return (
     <main>
       <Grid container spacing={0}>
@@ -14,7 +23,9 @@ export default function DefaultLayout() {
               <Typography variant="h6" component="div" sx={{ mr: "auto" }}>
                 OES-RSFG
               </Typography>
-              <Button color="inherit">Login</Button>
+              <IconButton color="inherit" onClick={colorMode.toggleColorMode}>
+                {theme.palette.mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
+              </IconButton>
             </Toolbar>
           </AppBar>
         </Grid>
