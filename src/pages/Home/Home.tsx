3c00e5c12397cac18dfe7ac1fbe96d66aecc3ec3
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2023-02-01 16:28:32 +0800

    [WIP] adding forms

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,46 @@
 import DefaultLayout from "@/layouts/DefaultLayout/DefaultLayout";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { Tab, Typography } from "@mui/material";
+import { Divider, Tab, Typography, useMediaQuery, useTheme } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import { Box } from "@mui/system";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+
+function ReportSickForm() {
+  return (
+    <Grid container>
+      <Grid sm={12}>
+        <Typography variant="h5">Personal Particulars</Typography>
+        <Divider />
+      </Grid>
+    </Grid>
+  );
+}
+
+function OutcomeForm() {
+  return null;
+}
+
+declare interface FormTabPanelProps {
+  children?: React.ReactNode;
+  value: string;
+}
+
+function FormTabPanel({ children, value }: FormTabPanelProps) {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
+  if (matches) {
+    return (
+      <TabPanel value={value} sx={{ px: 0 }}>
+        {children}
+      </TabPanel>
+    );
+  }
+  return <TabPanel value={value}>{children}</TabPanel>;
+}
 
 export default function Home() {
   const [tabFormValue, setTabFormValue] = useState<"report-sick" | "outcome">("report-sick");
+
   return (
     <DefaultLayout>
       <Grid container sx={{ my: 2 }}>
@@ -26,10 +60,12 @@ export default function Home() {
                 }}
               >
                 <Tab label="Report Sick" value="report-sick" />
-                <Tab label="Outcome" value="Outcome" />
+                <Tab label="Outcome" value="outcome" />
               </TabList>
-              <TabPanel value="report-sick">Report Sick</TabPanel>
-              <TabPanel value="outcome"></TabPanel>
+              <FormTabPanel value="report-sick">
+                <ReportSickForm />
+              </FormTabPanel>
+              <FormTabPanel value="outcome">Hello</FormTabPanel>
             </Box>
           </TabContext>
         </Grid>
