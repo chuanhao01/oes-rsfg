ace8206b4c03cc7d9cd0c1e24585d09489361234
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2023-02-24 10:59:58 +0800

    [fix] gh workflow var working in baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_CONFIG_BASE || "/",
+  base: process.env.WEBSITE_BASE || "/",
   plugins: [legacy(), react()],
   resolve: {
     alias: {
